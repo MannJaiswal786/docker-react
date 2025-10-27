1e9f78b91d0058146116adf864cfd5f4a80db554
Mann Jaiswal <mannjaiswal5@gmail.com>
2025-10-26 19:39:35 -0700

    Changed the paragraph text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chaning the application file to test the new branch
+          Changing the application file to test the new branch
         </p>
         <a
           className="App-link"
